f793a5b3a7e733d13cb58f5e415dd579b6dff6fe
nguyetnong <nguyetnong71099@gmail.com>
2022-10-23 19:10:00 +0700

    tinh tien

---
+++ tinh_tien.py
@@ -0,0 +1,10 @@
+num = int(input("nhap vao so tien can tinh:"))
+
+if num>=150:
+    print("so tien can tra la:",num-50)
+elif num>=100:
+    print("so tien can tra la:",num-25)
+elif num>=75:
+    print("so tien ca tra la:",num-15)
+else:
+    print("khong duoc giam gia va can tra la:",num)
